38896fa82f702b22ee92f970dd9c07be46dc7a37
Siara Leininger <leinings@oregonstate.edu>
2021-01-09 21:12:29 -0800

    Got filter working.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,8 +1,7 @@
 // from data.js
 var tableData = data;
 
-// Add all of the data to a table
-
+// Add all of the data to a table when the page loads
 // Select the table
 var tbody = d3.select("tbody");
 
@@ -15,7 +14,7 @@ tableData.forEach(ufoSighting => {
             var cell = row.append("td");
             cell.text(value);
     });       
-});
+})
 
 // Add filter functionality to button
 var filterButton = d3.select("#filter-btn");
@@ -24,6 +23,19 @@ filterButton.on("click", function() {
     // Get the input value in the filter field
     var dateInput = d3.select("#datetime").property("value");
 
+    // Clear the table so it will only display items that match filter
+    d3.selectAll("tr").remove();
+
     // Get filtered values
     var filteredData = tableData.filter(entry => entry.datetime === dateInput);
+
+    // Repopulate table with filtered data.
+    filteredData.forEach(function(filteredData) {
+        var row = tbody.append("tr");
+        // Add and populate the cells in the rows
+        Object.entries(filteredData).forEach(function([key, value]) {
+            var cell = row.append("td");
+            cell.text(value);
+    }); 
+    })
 })
