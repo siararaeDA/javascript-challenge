20ec8ff386b4a618ae744974e9fdf1a668649f0f
Siara Leininger <leinings@oregonstate.edu>
2021-01-09 20:55:22 -0800

    Adding functionality.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,22 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Add all of the data to a table
+
+// Select the table
+var tbody = d3.select("tbody");
+
+// Iterate through the data
+tableData.forEach(ufoSighting => {
+    // Add a row to the table
+    var row = tbody.append("tr");
+    // Add and populate cells in the row
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+            var cell = row.append("td");
+            cell.text(value);
+    });       
+});
+
+// Add filter functionality to button
+var filterButton = d3.select("#filter-btn");
+
